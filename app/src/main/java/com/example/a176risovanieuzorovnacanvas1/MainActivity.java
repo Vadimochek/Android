93d199cdfbb4893497c2d5323fac30910a36ed4a
Alexander <ddd>
2019-10-31 16:22:17 +0300

    1 версия,1.7.6

---
+++ app/src/main/java/com/example/a176risovanieuzorovnacanvas1/MainActivity.java
@@ -0,0 +1,19 @@
+package com.example.a176risovanieuzorovnacanvas1;
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.content.Context;
+import android.graphics.Canvas;
+import android.os.Bundle;
+import android.view.View;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(new MyView(this));
+    }
+
+}
+
+
