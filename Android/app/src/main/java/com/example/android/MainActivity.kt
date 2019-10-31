7de7f25ed85b4d86327ef44ef5d7941cbc9404c9
Vadimochek <vadim-132@mail.ru>
2019-11-01 01:51:58 +0300

    1.7.6 (1)

---
+++ Android/app/src/main/java/com/example/android/MainActivity.kt
@@ -0,0 +1,15 @@
+package com.example.android
+
+import androidx.appcompat.app.AppCompatActivity
+
+import android.os.Bundle
+
+
+class MainActivity : AppCompatActivity() {
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(MyView(this))
+    }
+
+}
---
+++ Android/app/src/main/java/com/example/android/MyView.java
@@ -0,0 +1,22 @@
+package com.example.android;
+import android.content.Context;
+import android.graphics.*;
+import android.view.View;
+
+public class MyView extends View {
+    public MyView(Context context) {
+        super(context);
+    }
+
+    @Override
+    protected void onDraw(Canvas canvas) {
+        super.onDraw(canvas);
+        Paint paint = new Paint();
+        int y = 0;
+        while (y < canvas.getHeight()) {
+            canvas.drawLine(0, y, this.getWidth(), y, paint);
+            y += 30;
+        }
+
+    }
+}
