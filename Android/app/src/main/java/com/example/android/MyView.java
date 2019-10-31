678120100a521fb36903b9adae7b23a239edf69f
Vadimochek <vadim-132@mail.ru>
2019-11-01 02:11:08 +0300

    1.7.6 (3)

--- Android/app/src/main/java/com/example/android/MyView.java
+++ Android/app/src/main/java/com/example/android/MyView.java
@@ -12,18 +12,24 @@ public class MyView extends View {
     protected void onDraw(Canvas canvas) {
         super.onDraw(canvas);
         Paint paint = new Paint();
+        paint.setColor(Color.BLUE);
+        paint.setStyle(Paint.Style.FILL);
+        canvas.drawRect(100,100, 300,300,paint);
+    }
+}
+        /* (2)
         paint.setColor(Color.YELLOW);
         paint.setStyle(Paint.Style.FILL);
         canvas.drawCircle(300,300,200,paint);
         paint.setColor(Color.RED);
         paint.setStyle(Paint.Style.STROKE);
         paint.setStrokeWidth(20);
-        canvas.drawCircle(300,300,200,paint);
-    }
+        canvas.drawCircle(300,300,200,paint);*/
+
 /* (1)
 int y = 0;
         while (y < canvas.getHeight()) {
             canvas.drawLine(0, y,
                 this.getWidth(), y, paint);
             y += 30;*/
-    }
+
