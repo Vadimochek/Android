8168a7b3ab4d42bc1fbc59ac74cc109f3eb20d77
Vadimochek <vadim-132@mail.ru>
2019-11-07 00:03:54 +0300

    1.8.7 Tochka

--- Android/app/src/main/java/com/example/android/MyView2.java
+++ Android/app/src/main/java/com/example/android/MyView2.java
@@ -7,36 +7,17 @@ public class MyView2 extends View {
     public MyView2(Context context) {
         super(context);
     }
-
+Paint paint = new Paint();
+    long lastTime = System.currentTimeMillis();
+    float x=0;
     @Override
     protected void onDraw(Canvas canvas) {
-        int x = 0;
-        Paint paint = new Paint();
-        paint.setColor(Color.BLACK);
-        canvas.drawPaint(paint);
-        paint.setColor(Color.CYAN);
-        while (x < getWidth()) {
-            canvas.drawLine(0, 1000, x, 0, paint);
-            x += 50;
-        }
-        x=0;
-            paint.setColor(Color.CYAN);
-            while (x < getWidth()) {
-                canvas.drawLine(x, getHeight(), getWidth(), 500, paint);
-                x += 50;
-            }
-            paint.setColor(Color.YELLOW);
-            canvas.drawCircle(500, 1000, 400, paint);
-            paint.setColor(Color.RED);
-            canvas.drawCircle(800, 1500, 100, paint);
-            paint.setColor(Color.GRAY);
-            canvas.drawCircle(700, 400, 150, paint);
-            paint.setColor(Color.BLUE);
-            canvas.drawCircle(200, 200, 100, paint);
-            paint.setColor(Color.WHITE);
-
-        }
-
+canvas.drawCircle(x,300,20,paint);
+long nowTime =System.currentTimeMillis();
+x+=0.01f*(nowTime-lastTime);
+lastTime=nowTime;
+        invalidate();
+    }
     }
 
 
