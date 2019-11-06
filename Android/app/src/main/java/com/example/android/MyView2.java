5bcfc2c19e9d6f766f47719e08a79def223aad56
Vadimochek <vadim-132@mail.ru>
2019-11-07 00:13:30 +0300

    1.8.7 Mnogo Tochek

--- Android/app/src/main/java/com/example/android/MyView2.java
+++ Android/app/src/main/java/com/example/android/MyView2.java
@@ -4,18 +4,34 @@ import android.graphics.*;
 import android.view.View;
 
 public class MyView2 extends View {
+    int N = 10;
+    float[] x  = new float[N];
+    float[] y  = new float[N];
+    float[] vx = new float[N];
+    float[] vy = new float[N];
     public MyView2(Context context) {
         super(context);
     }
 Paint paint = new Paint();
-    long lastTime = System.currentTimeMillis();
-    float x=0;
+    boolean started;
     @Override
     protected void onDraw(Canvas canvas) {
-canvas.drawCircle(x,300,20,paint);
-long nowTime =System.currentTimeMillis();
-x+=0.01f*(nowTime-lastTime);
-lastTime=nowTime;
+        if(!started) {
+            for (int i = 0; i < N; i++){
+                x[i] = (float)(Math.random() * 500);
+                y[i] = (float)(Math.random() * 500);
+                vx[i] = (float)(Math.random() * 6 - 3);
+                vy[i] = (float)(Math.random() * 6 - 3);
+            }
+            started = true;
+        }
+        for (int i = 0; i < N; i++) {
+            canvas.drawCircle(x[i], y[i], 20, paint);
+        }
+        for (int i = 0; i < N; i++) {
+            x[i] += vx[i];
+            y[i] += vy[i];
+        }
         invalidate();
     }
     }
