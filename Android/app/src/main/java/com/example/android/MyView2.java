d2fd5dca669c2782ce9282abeea194a399894b06
Vadimochek <vadim-132@mail.ru>
2019-11-07 01:21:37 +0300

    1.8.11 Upgrade Balls

--- Android/app/src/main/java/com/example/android/MyView2.java
+++ Android/app/src/main/java/com/example/android/MyView2.java
@@ -1,91 +1,65 @@
 package com.example.android;
 import android.content.Context;
 import android.graphics.Canvas;
-import android.graphics.Color;
+import android.graphics.*;
 import android.graphics.Paint;
-import android.os.CountDownTimer;
 import android.view.View;
 
 public class MyView2 extends View {
-
-    int N = 15;
-    int[] l = new int [N];
-    double x0, y0;
-    double[] x = new double [N];
-    double[] y = new double [N];
-    double g = 9.832f, pi = Math.PI;
-    double[] w = new double[N];
-    double fi0;
-    double[] fi = new double[N];
-    int t = 0, deltaT = 1;
-
-    void makePendulum()
-    {
-        fi0 = pi/4;
-
-        int l_min = 100;
-        for (int i = 0; i<N; i++)
-        {
-            l[i] = l_min;
-            l_min += 50;
-
-            w[i] = Math.sqrt(g/l[i]);
-        }
-    }
-    void movePendulum()
-    {
-        t += deltaT;
-
-        for (int i = 0; i<N; i++)
-        {
-            fi[i] = fi0 * Math.cos(w[i] * t);
-            x[i] = l[i]*Math.sin(fi[i]);
-            y[i] = l[i]*Math.cos(fi[i]);
-        }
-    }
-
-    MyView2(Context context) {
+    int N = 20;
+    float[] x  = new float[N];
+    float[] y  = new float[N];
+    float[] vx = new float[N];
+    float[] vy = new float[N];
+    float r;
+    public MyView2(Context context) {
         super(context);
-        makePendulum();
-        MyTimer timer = new MyTimer();
-        timer.start();
     }
+    Paint paint = new Paint();
+    boolean started;
 
     @Override
+
     protected void onDraw(Canvas canvas) {
-        x0 = getWidth()/2;
-        y0 = getHeight()/4;
-        Paint paint = new Paint();
-        canvas.drawCircle((float) x0, (float) y0, 10, paint);
-        for (int i = 0; i<N; i++)
-        {
+
+        if(!started) {
+          for (int i = 0; i < N; i++){
+        x[i] = (float)(Math.random() * 400);
+        y[i] = (float)(Math.random() * 400);
+        vx[i] = (float)(Math.random() * 20 - 3);
+        vy[i] = (float)(Math.random() * 20 - 3);
+        }
+        started = true;
+        }
+        for (int i = 0; i < N; i++) {
+           if (i%5==0) {r=(float)(Math.random() * 50);}
+            paint.setColor(Color.GREEN);
+        canvas.drawCircle(x[i], y[i], 20+r, paint);
+
+        }
+        for (int i = 0; i < N; i++) {
+        x[i] += vx[i];
+        y[i] += vy[i];
+        }
+        for (int i = 0; i < N; i++) {
+            if (x[i] < 0 || x[i] > this.getWidth()) {
+                vx[i] = -vx[i];
+                paint.setColor(Color.RED);
+                canvas.drawCircle(x[i], y[i], 20+5*r, paint);
+            }
+            if (y[i] < 0 || y[i] > this.getHeight()) {
+                vy[i] = -vy[i];paint.setColor(Color.RED);
+                canvas.drawCircle(x[i], y[i], 20+5*r, paint);
+            }
+        }
+        for (int i = 0; i < N - 1; i++) {
             paint.setColor(Color.BLUE);
-            canvas.drawLine((float)x0, (float)y0, (float)(x[i] + x0), (float)(y[i]+ y0), paint);
-            paint.setColor(Color.RED);
-            canvas.drawCircle((float)(x[i] + x0), (float)(y[i] + y0), 20, paint);
-        }
-    }
+            canvas.drawLine(x[i], y[i], x[i + 1], y[i + 1], paint);
 
-    void nextFrame()
-    {
-        movePendulum();
+        }
         invalidate();
-    }
+        }
+        }
 
-    class MyTimer extends CountDownTimer
-    {
-        MyTimer()
-        {
-            super(100000, 100);
-        }
-        @Override
-        public void onTick(long millisUntilFinished) {
-            nextFrame();
-        }
-        @Override
-        public void onFinish() {
-        }
-    }
-}
 
 
