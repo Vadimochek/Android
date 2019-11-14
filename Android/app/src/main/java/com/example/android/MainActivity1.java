5a315282dfabcba1ae4d861cd178f433e09aef10
Vadimochek <vadim-132@mail.ru>
2019-11-15 01:23:14 +0300

    Quadratic equation

--- Android/app/src/main/java/com/example/android/MainActivity1.java
+++ Android/app/src/main/java/com/example/android/MainActivity1.java
@@ -1,92 +1,62 @@
 package com.example.android;
+
 import android.app.Activity;
-import android.content.Context;
-import android.graphics.Color;
-import android.graphics.drawable.ColorDrawable;
+
 import android.os.Bundle;
-
-import android.view.LayoutInflater;
 import android.view.View;
-import android.view.View.OnClickListener;
-import android.view.View.OnLongClickListener;
-import android.widget.Button;
-import android.widget.GridLayout;
+import android.widget.EditText;
+import android.widget.TextView;
 
-import ru.samsung.itschool.book.cells.R;
+import ru.samsung.itschool.book.equation243.R;
 
-public class MainActivity1 extends Activity implements OnClickListener,
-        OnLongClickListener {
-
-    private int WIDTH = 4;
-    private int HEIGHT = 6;
-
-    private Button[][] cells;
-
+public class MainActivity1 extends Activity {
+    // Вызывается при создании Активности
     @Override
-    protected void onCreate(Bundle savedInstanceState) {
+    public void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.cells);
-        makeCells();
-
-        generate();
-
+        // Инициализирует Активность.
+        setContentView(R.layout.activity_main);
     }
 
-    void generate() {
-        for (int i = 0; i < HEIGHT; i++)
-            for (int j = 0; j < WIDTH; j++){
-                cells[i][j].setOnClickListener(this);
-                cells[i][j].setOnLongClickListener(this);
+    /** Вызывается при нажатии пользователем на кнопку Решить */
+    public void solveEquation(View view) {
+        // ax+b=c
+        double a = Double.parseDouble(((EditText)
+                findViewById(R.id.coefficient_a)).getText().toString());
+        double b = Double.parseDouble(((EditText)
+                findViewById(R.id.coefficient_b)).getText().toString());
+        double c = Double.parseDouble(((EditText)
+                findViewById(R.id.coefficient_c)).getText().toString());
+        TextView result = (TextView) findViewById(R.id.result);
+        if (a == 0 && b!=0) {
+            if ((c==0 && b<0)){
+                result.setText(" " + String.valueOf((Math.abs((-c) / b))));
             }
-    }
-
-    @Override
-    public boolean onLongClick(View v) {
-        return false;
-    }
-
-    @Override
-    public void onClick(View v) {
-        Button tappedCell = (Button) v;
-
-        int tappedX = getX(tappedCell);
-        int tappedY = getY(tappedCell);
-
-        for (int x = 0; x < WIDTH; x++)
-        {
-            cells[tappedY][x].setBackgroundColor(Color.RED);
-        }
-        for (int y = 0; y < HEIGHT; y++)
-        {
-            cells[y][tappedX].setBackgroundColor(Color.RED);
-        }
-
-    }
-
-
-    int getX(View v) {
-        return Integer.parseInt(((String) v.getTag()).split(",")[1]);
-    }
-
-    int getY(View v) {
-        return Integer.parseInt(((String) v.getTag()).split(",")[0]);
-    }
-
-    void makeCells() {
-        cells = new Button[HEIGHT][WIDTH];
-        GridLayout cellsLayout = (GridLayout) findViewById(R.id.CellsLayout);
-        cellsLayout.removeAllViews();
-        cellsLayout.setColumnCount(WIDTH);
-        for (int i = 0; i < HEIGHT; i++)
-            for (int j = 0; j < WIDTH; j++) {
-                LayoutInflater inflater = (LayoutInflater) getApplicationContext()
-                        .getSystemService(Context.LAYOUT_INFLATER_SERVICE);
-                cells[i][j] = (Button) inflater.inflate(R.layout.cell, cellsLayout, false);
-                cells[i][j].setOnClickListener(this);
-                cells[i][j].setOnLongClickListener(this);
-                cells[i][j].setTag(i + "," + j);
-                cellsLayout.addView(cells[i][j]);
+        else result.setText(" " + String.valueOf(((-c) / b)));
+        } else if (b == 0 && a!=0) {
+            if (c == 0) {
+                result.setText(" " + String.valueOf((0)));
+            } else {
+                result.setText(" " + String.valueOf((Math.sqrt(-c / a))) + "  " + String.valueOf((-Math.sqrt(-c / a))));
             }
+        }
+        else {
+            if (b * b - 4 * a * c >= 0) {
+                if (b*b-4*a*c==0) {
+
+                    result.setText(" " + String.valueOf(((-b + Math.sqrt(b * b - 4 * a * c)) / 2 * a)));
+                } else {
+                    result.setText(" " + String.valueOf(((-b + Math.sqrt(b * b - 4 * a * c)) / 2 * a)) + "  " + String.valueOf((-b - Math.sqrt(b * b - 4 * a * c)) / 2 * a));
+                }
+            }
+            else result.setText(" no solution");
+        }
+            if (a==0 && b==0 && c==0){
+                result.setText(" any x");
+            }
+            if (a==0 && b==0 && c!=0){
+                result.setText(" no solution");
+        }
     }
 
 }
