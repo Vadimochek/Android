2841dedac5a97c6659bae54047d55235106365f8
Vadimochek <vadim-132@mail.ru>
2019-11-18 00:46:16 +0300

    Log/Pas

--- Android/app/src/main/java/com/example/android/MainActivity1.java
+++ Android/app/src/main/java/com/example/android/MainActivity1.java
@@ -2,61 +2,53 @@ package com.example.android;
 
 import android.app.Activity;
 
+
 import android.os.Bundle;
 import android.view.View;
+
 import android.widget.EditText;
 import android.widget.TextView;
+import android.widget.Toast;
+
 
 import ru.samsung.itschool.book.equation243.R;
 
 public class MainActivity1 extends Activity {
-    // Вызывается при создании Активности
+    private EditText login;
+    private EditText password;
+
+
+
     @Override
     public void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         // Инициализирует Активность.
         setContentView(R.layout.activity_main);
+        login =  findViewById(R.id.log);
+        password =  findViewById(R.id.Pas);
+
     }
 
-    /** Вызывается при нажатии пользователем на кнопку Решить */
+    /**
+     * Вызывается при нажатии пользователем на кнопку Решить
+     */
     public void solveEquation(View view) {
-        // ax+b=c
-        double a = Double.parseDouble(((EditText)
-                findViewById(R.id.coefficient_a)).getText().toString());
-        double b = Double.parseDouble(((EditText)
-                findViewById(R.id.coefficient_b)).getText().toString());
-        double c = Double.parseDouble(((EditText)
-                findViewById(R.id.coefficient_c)).getText().toString());
-        TextView result = (TextView) findViewById(R.id.result);
-        if (a == 0 && b!=0) {
-            if ((c==0 && b<0)){
-                result.setText(" " + String.valueOf((Math.abs((-c) / b))));
-            }
-        else result.setText(" " + String.valueOf(((-c) / b)));
-        } else if (b == 0 && a!=0) {
-            if (c == 0) {
-                result.setText(" " + String.valueOf((0)));
-            } else {
-                result.setText(" " + String.valueOf((Math.sqrt(-c / a))) + "  " + String.valueOf((-Math.sqrt(-c / a))));
-            }
-        }
-        else {
-            if (b * b - 4 * a * c >= 0) {
-                if (b*b-4*a*c==0) {
+        TextView otvet1 =  findViewById(R.id.enter1);
+        TextView otvet2 =  findViewById(R.id.enter2);
 
-                    result.setText(" " + String.valueOf(((-b + Math.sqrt(b * b - 4 * a * c)) / 2 * a)));
-                } else {
-                    result.setText(" " + String.valueOf(((-b + Math.sqrt(b * b - 4 * a * c)) / 2 * a)) + "  " + String.valueOf((-b - Math.sqrt(b * b - 4 * a * c)) / 2 * a));
-                }
-            }
-            else result.setText(" no solution");
-        }
-            if (a==0 && b==0 && c==0){
-                result.setText(" any x");
-            }
-            if (a==0 && b==0 && c!=0){
-                result.setText(" no solution");
+        if (login.getText().toString().equals("1@1") && password.getText().toString().equals("123")) {
+
+           //Toast.makeText(getApplicationContext(), "Верно", Toast.LENGTH_SHORT).show();
+
+           otvet1.setText("Верно ");
+            otvet2.setText("");
+        } else {
+
+          // Toast.makeText(getApplicationContext(), "Вы ошиблись в логине или пароле", Toast.LENGTH_SHORT).show();
+           otvet2.setText("Вы ошиблись в логине или пароле");
+           login.setText("");
+           password.setText("");
+           otvet1.setText("");
         }
     }
-
 }
