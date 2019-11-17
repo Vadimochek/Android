72f951f17a9c1a4b1f63c32583368cc42eb180c4
Vadimochek <vadim-132@mail.ru>
2019-11-18 02:28:14 +0300

    2.5.3 USER

--- Android/app/src/main/java/com/example/android/MainActivity1.java
+++ Android/app/src/main/java/com/example/android/MainActivity1.java
@@ -14,9 +14,6 @@ import android.widget.Toast;
 import ru.samsung.itschool.book.equation243.R;
 
 public class MainActivity1 extends Activity {
-    private EditText login;
-    private EditText password;
-
 
 
     @Override
@@ -24,31 +21,10 @@ public class MainActivity1 extends Activity {
         super.onCreate(savedInstanceState);
         // Инициализирует Активность.
         setContentView(R.layout.activity_main);
-        login =  findViewById(R.id.log);
-        password =  findViewById(R.id.Pas);
+
 
     }
 
-    /**
-     * Вызывается при нажатии пользователем на кнопку Решить
-     */
-    public void solveEquation(View view) {
-        TextView otvet1 =  findViewById(R.id.enter1);
-        TextView otvet2 =  findViewById(R.id.enter2);
 
-        if (login.getText().toString().equals("1@1") && password.getText().toString().equals("123")) {
-
-           //Toast.makeText(getApplicationContext(), "Верно", Toast.LENGTH_SHORT).show();
-
-           otvet1.setText("Верно ");
-            otvet2.setText("");
-        } else {
-
-          // Toast.makeText(getApplicationContext(), "Вы ошиблись в логине или пароле", Toast.LENGTH_SHORT).show();
-           otvet2.setText("Вы ошиблись в логине или пароле");
-           login.setText("");
-           password.setText("");
-           otvet1.setText("");
-        }
-    }
 }
+
