e3ec5f5920b3f74abcb8b6b7450fd86839272e28
Unknown <vadim-132@mail.ru>
2020-03-05 01:11:31 +0300

    Sensor 3.5

--- Android/app/src/main/java/com/example/android/MainActivity1.java
+++ Android/app/src/main/java/com/example/android/MainActivity1.java
@@ -17,40 +17,18 @@ import ru.samsung.itschool.book.equation243.R;
 
 public class MainActivity1 extends Activity {
 
-    private EditText login;
-    private EditText password;
-    private Button log;
-
     @Override
     public void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        // Инициализирует Активность.
         setContentView(R.layout.activity_main);
-        login = findViewById(R.id.editText2);
-        password = findViewById(R.id.editText3);
-        log = findViewById(R.id.button3);
-
+        startService(new Intent(this, MyService.class));
+        // setContentView(new DrawView(this));
     }
-
-    public void Login(View view) {
-
-        // Если введенные логин и пароль будут словом "admin",
-        // показываем Toast сообщение об успешном входе:
-        if (login.getText().toString().equals("sams@mail") &&
-                password.getText().toString().equals("12345")) {
-            Toast.makeText(getApplicationContext(), "Вход выполнен!", Toast.LENGTH_SHORT).show();
-
-            // Выполняем переход на другой экран:
-            Intent intent = new Intent(MainActivity1.this, Game.class);
-            startActivity(intent);
-        }
-
-        // В другом случае выдаем сообщение с ошибкой:
-        else {
-            Toast.makeText(getApplicationContext(), "Неправильные данные!", Toast.LENGTH_SHORT).show();
-
-
+        @Override
+        protected void onDestroy() {
+            super.onDestroy();
+            stopService(new Intent(this, MyService.class));
         }
     }
-}
+
 
